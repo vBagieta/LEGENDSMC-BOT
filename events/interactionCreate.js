e8dddc399db2dbf8f9c9f69bb85446743a2f0d2a
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-05-09 14:30:09 +0200

    Show stack errors in error embed

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,46 +1,57 @@
 const { Events, EmbedBuilder, codeBlock } = require('discord.js');
+const { CombinedError, CombinedPropertyError } = require('@sapphire/shapeshift');
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+        const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				const errorEmbed = new EmbedBuilder()
-                	.setTitle('Wystąpił błąd.')
-					.setColor('Red')
-					.setDescription('Zgłoś ten błąd deweloperowi')
-                	.addFields(
-                    	{ name: 'ERROR', value: codeBlock(`${error}`), inline: true },
-                	)
-                	.setTimestamp()
-					.setFooter({ text: `${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL({ dynamic: true })}` })
-
-                await interaction.reply({ embeds: [errorEmbed], ephemeral: true })
-			} else {
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
                 const errorEmbed = new EmbedBuilder()
-                	.setTitle('Wystąpił błąd.')
-					.setColor('Red')
-					.setDescription('Zgłoś ten błąd deweloperowi')
-                	.addFields(
-                    	{ name: 'ERROR', value: codeBlock(`${error}`), inline: true },
-                	)
-                	.setTimestamp()
-					.setFooter({ text: `${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL({ dynamic: true })}` })
+                    .setTitle('Wystąpił błąd.')
+                    .setColor('Red')
+                    .setDescription('Zgłoś ten błąd deweloperowi')
+                    .addFields(
+                        { name: 'ERROR', value: codeBlock(error.stack || error), inline: true },
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: `${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL({ dynamic: true })}` })
 
                 await interaction.reply({ embeds: [errorEmbed], ephemeral: true })
-			}
-		}
-	},
+            } else {
+                let errorMessage;
+                if (error instanceof CombinedError) {
+                    errorMessage = error.errors.map(err => err.message).join('\n');
+                } else if (error instanceof CombinedPropertyError) {
+                    errorMessage = error.errors.map(err => err[0].message).join('\n');
+                } else {
+                    errorMessage = error.stack || error;
+                }
+
+                const errorEmbed = new EmbedBuilder()
+                    .setTitle('Wystąpił błąd.')
+                    .setColor('Red')
+                    .setDescription('Zgłoś ten błąd deweloperowi')
+                    .addFields(
+                        { name: 'ERROR', value: codeBlock(error) },
+						{ name: 'STACK ERROR', value: codeBlock(errorMessage) },
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: `${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL({ dynamic: true })}` })
+
+                await interaction.reply({ embeds: [errorEmbed], ephemeral: true })
+            }
+        }
+    },
 };
