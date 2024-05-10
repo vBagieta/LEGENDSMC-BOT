74cd13f0cba05ce5cc5398641cdc50256f71653d
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-05-10 17:44:22 +0200

    Remove debug message and replace embed with message in AntyInvite

--- events/antyInvite.js
+++ events/antyInvite.js
@@ -16,23 +16,18 @@ module.exports = {
             } else {
                 const member = await message.guild.members.fetch(message.author.id);
                 if (member.permissions.has('KICK_MEMBERS')) {
-                    console.log('[INFO] Ignoring ' + message.author.username + '\'s invite. REASON: User has [KICK_MEMBER] permission.');
                     return;
                 } else {
-                    const embed = new EmbedBuilder()
-                        .setDescription(`<@${message.author.id}>, nie wysyłaj zaproszeń!`)
-                        .setColor('Red');
                     try {
-                        const sentEmbed = await message.channel.send({ embeds: [embed], ephemeral: true });
+                        const sentMessage = await message.channel.send(`<@${message.author.id}>, nie wysyłaj zaproszeń!`);
                         setTimeout(() => {
-                            sentEmbed.delete().catch(console.error);
+                            sentMessage.delete().catch(console.error);
                         }, 10000);
                     } catch (error) {
                         console.error(error);
                     }
                     try {
                         await message.delete();
-                        console.log('[INFO] Deleting ' + message.author.username + '\'s invite. INVITE-MATCH: ' + inviteMatch)
                     } catch (error) {
                         console.error(error);
                     }
