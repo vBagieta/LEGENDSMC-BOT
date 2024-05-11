ac381016e7a7dba600e4abf75d4da51160f16b54
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-05-11 21:59:49 +0200

    Added last 10 messages from ticket to log

--- events/ticket/confirmTicketClosing.js
+++ events/ticket/confirmTicketClosing.js
@@ -1,6 +1,10 @@
 const { Events, EmbedBuilder, time, TimestampStyles } = require('discord.js');
 const { ticketLogsChannelId } = require('../../configs/main.json');
 
+function isBot(message) {
+    return message.author.bot;
+}
+
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
@@ -16,25 +20,37 @@ module.exports = {
             const timer = setTimeout(async () => {
                 const channel = interaction.client.channels.cache.get(interaction.channelId);
                 if (channel) {
+
+                    const messages = await channel.messages.fetch({ limit: 10 });
+
+                    const botMessages = messages.filter(isBot);
+                    botMessages.forEach(message => message.delete());
+
+                    let lastMessages = "**BRAK OSTATNICH WIADOMOŚĆI**";
+                    const userMessages = messages.filter(message => !isBot(message));
+                    if (userMessages.size > 0) {
+                        lastMessages = userMessages.map(msg => `${msg.author.tag}: ${msg.content}`).join('\n');
+                    }
+
                     await channel.delete();
+                    
+                    const [author, id] = channel.name.split("-");
+
+                    const deletedTicketEmbed = new EmbedBuilder()
+                        .setTitle('Ticket LOG')
+                        .setColor('Red')
+                        .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                        .setDescription(`Ostatnie 10 wiadomości z kanału:\n${lastMessages}`)
+                        .addFields(
+                            { name: 'AUTOR TICKETA', value: `<@${id}> (${author})` },
+                            { name: 'ZAMKNIĘTO', value: `${time(new Date(), TimestampStyles.RelativeTime)}` },
+                            { name: 'PRZEZ', value: `<@${interaction.user.id}>` },
+                        )
+                        .setTimestamp()
+                        .setFooter({ text: 'System ticketów' });
+                
+                    interaction.guild.channels.cache.get(ticketLogsChannelId).send({ embeds: [deletedTicketEmbed] });
                 }
-
-                const [author, id] = channel.name.split("-");
-
-                const deletedTicketEmbed = new EmbedBuilder()
-                    .setTitle('Ticket LOG')
-                    .setColor('Red')
-                    .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-                    .setDescription(`Użytkownik <@${interaction.user.id}> zamknął ticket.`)
-                    .addFields(
-                        { name: 'AUTOR', value: `<@${id}> (${author})` },
-                        { name: 'ZAMKNIĘTO', value: `${time(new Date(), TimestampStyles.RelativeTime)}` }
-                    )
-                    .setTimestamp()
-                    .setFooter({ text: 'System ticketów' })
-            
-                interaction.guild.channels.cache.get(ticketLogsChannelId).send({ embeds: [deletedTicketEmbed] })
-
             }, 30000);
             
         } else if (interaction.customId === 'cancelTicketDeletion') {
@@ -42,7 +58,7 @@ module.exports = {
                 content: 'Anulowano zamykanie ticketa.',
                 components: [],
                 ephemeral: true
-            })
+            });
         }
     }
 };
