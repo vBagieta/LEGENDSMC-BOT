6cd32026ce06657a49d0f717c9db6330e2edbd34
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-05-13 15:07:36 +0200

    Fix bot crash in AutoBan

--- events/systems/autoBan.js
+++ events/systems/autoBan.js
@@ -5,34 +5,36 @@ module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
 
-        const member = await message.guild.members.fetch(message.author.id);
-        if (member.permissions.has(PermissionFlagsBits.KickMembers)) {
-            return;
-        }
-
-        if (message.channel.id === autoBanChannelId) {
-            try {
-
-                await message.guild.members.ban(message.author,{
-                    reason: '[AutoBan]',
-                    deleteMessageSeconds: 3600
-                  });
-                
-                const logEmbed = new EmbedBuilder()
-                    .setTitle('AutoBan LOG')
-                    .setColor('Red')
-                    .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
-                    .setDescription(`Użytkownik <@${message.author.id}> wysłał wiadomość na kanale zabezpieczającym.`)
-                    .addFields(
-                        { name: 'WIADOMOŚĆ', value: codeBlock(message.content) }
-                    )
-                    .setTimestamp()
-                    .setFooter({ text: 'LEGENDSMC - System' })
-
-                message.guild.channels.cache.get(logsChannelId).send({ embeds: [logEmbed] })
-            } catch (error) {
-                console.error(error);
+        try {
+            const member = await message.guild.members.fetch(message.author.id);
+            if (member.permissions.has(PermissionFlagsBits.KickMembers)) {
+                return;
             }
-        }
+
+            if (message.channel.id === autoBanChannelId) {
+                try {
+
+                    await message.guild.members.ban(message.author, {
+                        reason: '[AutoBan]',
+                        deleteMessageSeconds: 3600
+                    });
+
+                    const logEmbed = new EmbedBuilder()
+                        .setTitle('AutoBan LOG')
+                        .setColor('Red')
+                        .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+                        .setDescription(`Użytkownik <@${message.author.id}> wysłał wiadomość na kanale zabezpieczającym.`)
+                        .addFields(
+                            { name: 'WIADOMOŚĆ', value: codeBlock(message.content) }
+                        )
+                        .setTimestamp()
+                        .setFooter({ text: 'LEGENDSMC - System' })
+
+                    message.guild.channels.cache.get(logsChannelId).send({ embeds: [logEmbed] })
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        } catch (error) {}
     }
 };
