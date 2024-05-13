a60c8c78523500ffc2d9341a7e4271879e0a31d9
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-05-13 15:03:17 +0200

    Added deleted ticket channel to TicketSystem logs

---
+++ events/ticket/deletedTicketChannel.js
@@ -0,0 +1,31 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const { ticketCategoryId, ticketLogsChannelId } = require('../../configs/main.json');
+
+module.exports = {
+    name: Events.ChannelDelete,
+    async execute(channel) {
+        if (channel.parent && channel.parent.id === ticketCategoryId) {
+            const channelDeleteId = channel.id;
+            channel.guild.fetchAuditLogs({ type: 12 })
+                .then(logs => {
+                    const entry = logs.entries.find(entry => entry.target.id === channelDeleteId);
+                    if (entry) {
+                        const author = entry.executor;
+                        const deletedTicketEmbed = new EmbedBuilder()
+                            .setTitle('LOG Zgłoszeń')
+                            .setColor('DarkRed')
+                            .setDescription(`<@${author.id}> usunął ręcznie kanał zgłoszenia.`)
+                            .addFields(
+                                { name: 'ID ZGŁOSZENIA', value: channel.name }
+                            )
+                            .setTimestamp()
+                            .setFooter({ text: 'System zgłoszeń', iconURL: channel.guild.iconURL({ dynamic: true }) });
+        
+
+                        channel.guild.channels.cache.get(ticketLogsChannelId).send({ embeds: [deletedTicketEmbed] })
+                    }
+                }
+            )
+        }
+    }
+};
