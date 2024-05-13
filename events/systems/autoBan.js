972c5246a814dca0d9223cd21dd20c900d776195
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-05-13 16:15:29 +0200

    Changed footer of embeds in /events/systems when possible

--- events/systems/autoBan.js
+++ events/systems/autoBan.js
@@ -28,7 +28,7 @@ module.exports = {
                             { name: 'WIADOMOŚĆ', value: codeBlock(message.content) }
                         )
                         .setTimestamp()
-                        .setFooter({ text: 'LEGENDSMC - System' })
+                        .setFooter({ text: 'System', iconURL: interaction.guild.iconURL({ dynamic: true }) });
 
                     message.guild.channels.cache.get(logsChannelId).send({ embeds: [logEmbed] })
                 } catch (error) {
