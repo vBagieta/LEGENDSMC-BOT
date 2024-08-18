cf81786215409a5ce439e12a07ca9bd320867e9a
Krystian Bochenek <87995539+vBagieta@users.noreply.github.com>
2024-08-18 14:49:35 +0200

    Updated /rules command

--- commands/utility/rules.js
+++ commands/utility/rules.js
@@ -17,90 +17,61 @@ module.exports = {
                 .setDescription('Oznaczyć regulamin jako zaktualizowany?'))
         .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
 
-    async execute(interaction) { 
+    async execute(interaction) {
         const updated = interaction.options.getBoolean('update') ?? false;
-        const filePath = path.resolve(__dirname, '../../configs/dates.json');
+        const timestampPath = path.resolve(__dirname, '../../configs/dates.json');
+        const rulesPath = path.resolve(__dirname, '../../configs/rules.txt');
         let updatedDate;
+        let rules;
 
         if (updated) {
-            updatedDate = time(Math.floor(Date.now() / 1000), TimestampStyles.RelativeTime);
+            const rulesTimestamp = new Date().toISOString();
+            fs.writeFileSync(timestampPath, JSON.stringify({ rulesTimestamp }), 'utf8');
+        }
 
-            fs.readFile(filePath, 'utf8', (error, data) => {
-                if (error) {
-                    console.error('Błąd przy odczytywaniu pliku:', error);
-                    return;
-                }
+        if (fs.existsSync(timestampPath)) {
+            const rawData = fs.readFileSync(timestampPath, 'utf8');
+            const timestamp = JSON.parse(rawData);
+            date = timestamp.rulesTimestamp;
+        }
 
-                let jsonData;
-                try {
-                    jsonData = JSON.parse(data);
-                } catch (parseError) {
-                    console.error('Błąd przy parsowaniu pliku JSON:', parseError);
-                    return;
-                }
-
-                jsonData.rulesUpdateTimestamp = updatedDate;
-
-                const updatedJsonData = JSON.stringify(jsonData, null, 2);
-                fs.writeFile(filePath, updatedJsonData, (writeErr) => {
-                    if (writeErr) {
-                        console.error('Błąd przy zapisywaniu pliku:', writeError);
-                    } else {
-                        console.log('Dane zostały zaktualizowane i zapisane do pliku:', filePath);
-                    }
-                });
-            });
+        if (fs.existsSync(rulesPath)) {
+            rules = fs.readFileSync(rulesPath, 'utf8');
         } else {
-            try {
-                const data = fs.readFileSync(filePath, 'utf8');
-                const jsonData = JSON.parse(data);
-                updatedDate = jsonData.rulesUpdateTimestamp;
-            } catch (error) {
-                console.error(error);
-                return;
+            rules = 'Nie odnaleziono zawartości regulaminu!';
+        }
+        
+        const rulesEmbed = new EmbedBuilder()
+            .setDescription(rules)
+            .setColor('Red');
+
+        const warningEmbed = new EmbedBuilder()
+        .setDescription(
+            '\n\n**Regulamin z dnia:** ' + time(new Date(date), TimestampStyles.LongDateTime)
+        )
+        .setFooter({ text: 'Zakazane jest powielanie niniejszych zasad bez zgody odpowiednich osób zarządzających serwisem. Ten regulamin jest własnością serwisu LEGENDSMC.PL' })
+        .setColor('Red')
+
+        const channel = interaction.guild.channels.cache.get(rulesChannelId);
+        if (!channel) {
+            await interaction.reply({
+                content: 'Nie znaleziono kanału.',
+                ephemeral: true
+            });
+            return;
+        }
+
+        const messages = await channel.messages.fetch({ limit: 3 });
+        if (messages.size > 0) {
+            for (const message of messages.values()) {
+                await message.delete();
             }
         }
 
-        const rulesEmbed = new EmbedBuilder()
-            .setAuthor({
-                name: 'REGULAMIN SERWERA LEGENDSMC.PL',
-                iconURL: 'https://i.imgur.com/GNSNUOS.png'
-            })
-            .setColor('Red')
-            .setDescription(
-                '**Art. 1 Postanowienia ogólne**\n' +
-                '**§1** Każdy użytkownik w chwili dołączenia na serwer automatycznie akceptuje poniższy regulamin.\n' +
-                '**§2** Nie znajomość regulaminu lub jego nie zrozumienie nie zwalnia z jego nie przestrzegania.\n' +
-                '**§3** Administrator może ukarać gracza za przewinienia nie uwzględnione w tym regulaminie, jeżeli uzna to za stosowne.\n' +
-                '**§4** Administracja zastrzega sobie prawo do edycji regulaminu bez wcześniejszego poinformowania użytkowników.\n' +
-                '**§5** Ten regulamin jest własnością serwera LEGENDSMC.PL.\n\n' +
-                '**Art. 2 Serwer Discord**\n' +
-                '**§1** Na kanałach tekstowych jest obowiązek przestrzegania zasad kultury osobistej.\n' +
-                '**§2** Zakaz publikowania wrażliwych danych m.in: adres zamieszkania, zdjęcia.\n' +
-                '**§3** Zakaz obrażania, prowokowania itp. innych użytkowników serwera.\n' +
-                '**§4** Zakaz publikowania treści powszechnie uważanych za nieetyczne.\n' +
-                '**§5** Zakaz podszywania się pod administrację serwera oraz jego użytkowników.\n' +
-                '**§6** Zakaz reklamowania innych serwerów gry Minecraft.\n' +
-                '**§7** Użytkownik jak i administracja jest zobowiązany do przestrzegania zasad Discorda.\n\n' +
-                '**Art. 3 Serwer Minecraft**\n' +
-                '**§1** Gracz ma obowiązek zgłaszać wszelkie błędy na serwerze.\n' +
-                '**§2** Zakaz obrażania, prowokowania itp. innych graczy.\n' +
-                '**§3** Zakaz używania niedozwolonych modyfikacji, cheatów czy klientów.\n' +
-                '**§4** Zakaz podszywania się pod administrację serwera oraz jego graczy.\n\n' +
-                '**Ostatnia aktualizacja:** ' + updatedDate
-            );
-
-        const channel = interaction.guild.channels.cache.get(rulesChannelId);
-        const messages = await channel.messages.fetch({ limit: 1 });
-        
-        if (messages.size === 1) {
-            await messages.first().delete();
-        }
-
-        await channel.send({ embeds: [rulesEmbed] });
+        await channel.send({ content: '## REGULAMIN SERWISU LEGENDSMC.PL', embeds: [rulesEmbed, warningEmbed] });
         await interaction.reply({
-            content: `Pomyślnie wysłano panel na kanał ` + channelMention(channel.id),
-            ephemeral: true 
+            content: 'Pomyślnie wysłano regulamin na kanał ' + channelMention(channel.id),
+            ephemeral: true
         });
     },
 };
